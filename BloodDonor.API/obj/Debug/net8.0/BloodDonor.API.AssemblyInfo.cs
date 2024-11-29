15dc85f7a538f010960189adb2100ccde5d8a518
Pedro <pdro5k@outlook.com>
2024-11-28 21:27:35 -0300

    Initial

--- BloodDonor.API/obj/Debug/net8.0/BloodDonor.API.AssemblyInfo.cs
+++ BloodDonor.API/obj/Debug/net8.0/BloodDonor.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonor.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc760b72f10c616ffab5d6b8ef9512269fa8221d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonor.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonor.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
